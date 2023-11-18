969933d19ec5cfaee42d6019e0bf43f4b557b26a
abhinov-gogoi <abhinav.gogoi@clairvoyantsoft.com>
2023-11-18 08:46:50 +0530

    refactored code

---
+++ file-handling/app_refactored.py
@@ -0,0 +1,186 @@
+# Student details Project
+# CRUD Operations - create, read, update, delete
+import os
+
+# create the file
+main_file = open('student_details.txt', 'a')
+prompt = ("\nWelcome to SMS - Student Management System."
+          "\nPlease type 1, 2, 3, 4"
+          "\n1 - Add Record (Create)"
+          "\n2 - Display All Records (Read)"
+          "\n3 - Search student record by name (Read)"
+          "\n4 - Search student record by roll no (Read)"
+          "\n5 - Delete student record by name (Delete)"
+          "\n6 - Update student record by name (Update)"
+          "\n7 - Exit")
+
+
+def add_student_details():
+    print("\nEnter student details")
+    name = input('\nEnter student name: ')
+    roll_no = input('\nEnter student roll no: ')
+    student_class = input('\nEnter student class: ')
+    fees = input('\nEnter student fees: ')
+    percentage = input('\nEnter student percentage: ')
+    print("You have entered the following student details ...")
+    print('Student Name : ', name)
+    print('Student Roll No : ', roll_no)
+    print('Student Class : ', student_class)
+    print('Student Fees : ', fees)
+    print('Student Percentage : ', percentage)
+    file = open('student_details.txt', 'a')
+    file.write('\n' + name + '~~~' + roll_no + '~~~' + student_class + '~~~' + fees + '~~~' + percentage + '\n')
+    file.close()
+
+
+def get_all_records():
+    print("\nAll Student Records\n")
+    print("\nNAME~~~RollNo~~~Class~~~Fees~~~Percentage\n")
+    file = open('student_details.txt', 'r')
+    while True:
+        line_content = file.readline()
+        length = len(line_content)
+        if length == 0:
+            break
+        print(line_content.strip())
+    file.close()
+
+
+def search_student_by_name():
+    print("\nSearching for a student record by name")
+    search_string = input("\nPlease enter the student name: ")
+    file = open('student_details.txt', 'r')
+    while True:
+        line_content = file.readline()
+        length = len(line_content)
+        if length == 0:
+            print("Record not found")
+            break
+
+        my_list = line_content.split("~~~")
+        if my_list[0] == search_string:
+            print("\nRecord found")
+            print("\n Student name : ", my_list[0])
+            print("\n Student rollno : ", my_list[1])
+            print("\n Student class : ", my_list[2])
+            print("\n Student fees : ", my_list[3])
+            print("\n Student percentage : ", my_list[4])
+            break
+    file.close()
+
+
+def search_student_by_roll_no():
+    print("\nSearching for a student record by roll_no")
+    search_string = input("Please enter the student roll no: ")
+    file = open('student_details.txt', 'r')
+    while True:
+        line_content = file.readline()
+        length = len(line_content)
+        if length == 0:
+            print("Record not found")
+            break
+
+        my_list = line_content.split("~~~")
+        if my_list[1] == search_string:
+            print("\nRecord found")
+            print("\n Student name : ", my_list[0])
+            print("\n Student roll_no : ", my_list[1])
+            print("\n Student class : ", my_list[2])
+            print("\n Student fees : ", my_list[3])
+            print("\n Student percentage : ", my_list[4])
+            break
+    file.close()
+
+
+def delete_student_by_name():
+    print("\nDeleting a student details by name")
+    search_string = input("Please enter the student name: ")
+    file = open('student_details.txt', 'r')
+    temp_file = open('temp_file.txt', 'w')
+    student_exist = False
+    while True:
+        line_content = file.readline()
+        if len(line_content) == 0:
+            break
+
+        name = line_content.split("~~~")[0]
+        if name == search_string:
+            student_exist = True
+            pass
+        else:
+            temp_file.write(line_content)
+    file.close()
+    temp_file.close()
+    if student_exist:
+        print("Student Found. Delete Successful")
+    else:
+        print("Student not found. Delete Unsuccessful")
+    os.remove('student_details.txt')
+    os.rename('temp_file.txt', 'student_details.txt')
+
+
+def update_student_details():
+    print("\nUpdating a student details by name")
+    search_string = input("Please enter the student name: ")
+    file = open('student_details.txt', 'r')
+    temp_file = open('temp_file.txt', 'w')
+    while True:
+        line_content = file.readline()
+        if len(line_content) == 0:
+            break
+
+        name = line_content.split("~~~")[0]
+        if name == search_string:
+            print("\nEnter new student details")
+            name = input('\nEnter new student name: ')
+            roll_no = input('\nEnter new student roll no: ')
+            student_class = input('\nEnter new student class: ')
+            fees = input('\nEnter new student fees: ')
+            percentage = input('\nEnter new student percentage: ')
+
+            print("You have updated the following student details ...")
+            print('Student Name : ', name)
+            print('Student Roll No : ', roll_no)
+            print('Student Class : ', student_class)
+            print('Student Fees : ', fees)
+            print('Student Percentage : ', percentage)
+
+            temp_file.write(
+                name + '~~~' + roll_no + '~~~' + student_class + '~~~' + fees + '~~~' + percentage + '\n')
+            print("Student details updated")
+            break
+        else:
+            temp_file.write(line_content)
+    file.close()
+    temp_file.close()
+    os.remove('student_details.txt')
+    os.rename('temp_file.txt', 'student_details.txt')
+
+
+while True:
+    print(prompt)
+
+    # take input from user
+    user_input = input('Please give your input: ')
+    print(user_input)
+
+    if user_input == '7':
+        break
+
+    elif user_input == '1':
+        add_student_details()
+
+    elif user_input == '2':
+        get_all_records()
+
+    elif user_input == '3':
+        search_student_by_name()
+
+    elif user_input == '4':
+        search_student_by_roll_no()
+
+    elif user_input == '5':
+        delete_student_by_name()
+
+    elif user_input == '6':
+        update_student_details()
