6e8c2d330810551d6c4eaef1484b5705d5a85d3d
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2023-11-27 13:21:04 +0530

    python problem solving assignment is in progress-day1

--- problem-solving/day1/two-functions-intro.py
+++ problem-solving/day1/two-functions-intro.py
@@ -1,60 +1,50 @@
 # Write a python function named greet that prints 'hello world' on the console
+def greet():
+    print('hello world')
 
 
-
-
-
-
-
-
-
-
-
+greet()
 
 
 # Write a python function that takes in two numbers and prints the sum
 
+def two_numbers(x, y):
+    print(int(x) + int(y))
 
 
-
-
-
-
-
+two_numbers(8, 9)
 
 
 # Write a python function that takes two numbers and returns the sum
 
+def two_numbers(x, y):
+    result = int(x) + int(y)
+    return result
 
 
-
-
-
-
+print(two_numbers(5, 6))
 
 
 # Write a python function that takes a string and prints string in uppercase
 
+def user_input(string):
+    print(string.upper())
 
 
-
-
-
-
-
-
-
+user_input("hello")
 
 # Write a python function that takes a string and returns the string in uppercase
 
 
+def user_input(string):
+    result = string.upper()
+    return result
 
 
-
-
-
-
+print(user_input("my name is nabasmita"))
 
 # Write a python function that asks the user for his name and then prints the name
 
+x = input("Enter your name: ")
+print(x)
 
