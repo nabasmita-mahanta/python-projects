e5c030f79ec35ae7a9574c7de8a181cccdd410dd
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2023-11-27 12:02:45 +0530

    python problem solving assignment is in progress-day1

--- problem-solving/day1/one-hello-world.py
+++ problem-solving/day1/one-hello-world.py
@@ -14,7 +14,9 @@ print('hello world')
 
 
 # Write a python program to take user input name
+name = input('Please enter your name: ')
 # and print hello 'name' to the console
+print('hello ' + name)
 
 
 
@@ -30,4 +32,16 @@ print('hello world')
 
 
 # Take two numbers as input
+x = input("Please enter first number: ")
+y = input("Please enter second number: ")
 # and then print the sum, difference, power, division, floor-division of the numbers
+sum = int(x)+int(y)
+print (" Sum of two number: ", sum)
+difference = int(x)-int(y)
+print("Difference of two number: ", difference)
+power = int(x)**int(y)
+print("Power of two number: ", power)
+division = int(x)/int(y)
+print("Division of two number: ", division)
+floor_div = int(x)//int(y)
+print("Floor division of two number: ",floor_div)
