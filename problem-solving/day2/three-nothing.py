d8080f6d70d9bac98fdb7a520ed437100ff30e07
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2023-11-28 10:57:09 +0530

    python problem solving assignment is in progress-day2

--- problem-solving/day2/three-nothing.py
+++ problem-solving/day2/three-nothing.py
@@ -1,1 +1,7 @@
 # Write a function named 'do_nothing' that does absolutely NOTHING
+
+def do_nothing():
+    pass
+
+
+do_nothing()
--- problem-solving/day2/two-loops.py
+++ problem-solving/day2/two-loops.py
@@ -1,74 +1,54 @@
 # Write a function that prints from 1 to 100 using for loop
 
+def numbers():
+    for number in range(1, 101):
+        print(number)
 
 
-
-
-
-
-
+numbers()
 
 
 # Write a function that prints from 100 to 1 using for loop
+def numbers():
+    for number in reversed(range(1, 101)):
+        print(number)
 
 
-
-
-
-
-
-
+numbers()
 
 
 # Write a function that prints from 1 to 100 and skipping 3 numbers in between
 # eg - 1, 5, 9, 13 ... and so on
 
+def new_numbers():
+    for number in range(1, 101, 4):
+        print(number)
 
 
-
-
-
-
-
-
-
+new_numbers()
 
 
 # Write a function that prints from 100 to 1 using while loop
 
+def numbers(x):
+    while x > 0:
+        print(x)
+        x = x - 1
 
 
-
-
-
-
-
+numbers(100)
 
 
 # Write a function that prints from 100 to 1 using do-while loop
-
-
-
-
-
-
-
-
-
+# There is no do-while loop in python.
 
 
 # Write a function that prints only EVEN numbers from 1 to 100
 
+def even_numbers():
+    for num in range(1, 101):
+        if num % 2 == 0:
+            print(num)
 
 
-
-
-
-
-
-
-
-
-
-
-
+even_numbers()
