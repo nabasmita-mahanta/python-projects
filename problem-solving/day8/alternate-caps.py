7e075a59b88c3a90011611835feea5b754cfbbf6
abhinov-gogoi <abhinav.gogoi@clairvoyantsoft.com>
2023-12-07 23:24:15 +0530

    python problem-solving day8 added

---
+++ problem-solving/day8/alternate-caps.py
@@ -0,0 +1,28 @@
+# Alternate Caps
+
+"""
+Given a string, Write a function that capitalises every alternate letter
+# example - Nabasmita is transformed to NaBaSmItA
+
+
+Examples:
+
+Input : Nabasmita
+Output : NaBaSmItA
+
+Input : I_love_python
+Output : I_LoVe_pYtHoN
+
+
+"""
+
+word = input('Enter any word: ')
+
+
+def capitalise(string):
+    # TODO :: Complete this function
+    return string
+
+
+result_string = capitalise(word)
+print(result_string)
