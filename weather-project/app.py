0479786320467b4a922e4f971440d5047865a2c4
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2024-02-07 20:25:29 +0530

    Milestone project for weather analysis is going on

--- weather-project/app.py
+++ weather-project/app.py
@@ -8,25 +8,25 @@ for city in cities:
     weather_data = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}")
 
-    print(weather_data.json())
     result = weather_data.json()
-    print(type(result))
-    print(result["coord"])
+    print(result)
 
+
+def generate_insert_query(result):
     latitude = result["coord"]["lat"]
     longitude = result["coord"]["lon"]
 
     weather_main = result["weather"][0]["main"]
     weather_description = result["weather"][0]["description"]
 
-    temperature = result["main"]["temp"]
-    temperature_feels_like = result["main"]["feels_like"]
-    temperature_min = result["main"]["temp_min"]
-    temperature_max = result["main"]["temp_max"]
-    pressure = result["main"]["pressure"]
-    humidity = result["main"]["humidity"]
+    temperature = int(result["main"]["temp"])
+    temperature_feels_like = int(result["main"]["feels_like"])
+    temperature_min = int(result["main"]["temp_min"])
+    temperature_max = int(result["main"]["temp_max"])
+    pressure = int(result["main"]["pressure"])
+    humidity = int(result["main"]["humidity"])
 
-    wind_speed = result["wind"]["speed"]
+    wind_speed = int(result["wind"]["speed"])
 
     date = result["dt"]
 
@@ -36,3 +36,6 @@ for city in cities:
 
     timezone = result["timezone"]
     city_name = result["name"]
+
+    query = (f'INSERT INTO table_name (city_name, temp, temp_feels_like,weather_main,weather_desc,temp_min,temp_max,pressure, humidity, wind_speed, date, country, sunrise, sunset) '
+             f'VALUES({city_name},{temperature},{temperature_feels_like},{weather_main},{weather_description},{temperature_min},{temperature_max},{pressure},{humidity},{wind_speed},null,{country},null,null);')
