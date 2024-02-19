229b5fddbe98f3bb3e7a4276fb94e3d948644228
abhinov-gogoi <abhinav.gogoi@clairvoyantsoft.com>
2024-02-19 11:40:30 +0530

    optimise the app code

--- weather-project/app.py
+++ weather-project/app.py
@@ -1,26 +1,42 @@
 import requests
 import helper
 import mysql.connector as sql
+import config
 
-cities = ['Delhi', 'Bangalore', 'Mumbai', 'Kolkata', 'Chennai', 'Guwahati', 'Hyderabad', 'Pune', 'Jaipur']
-api_key = '3f9539b62b62177d2d723e8b117b9f2e'
+prop = config.PROPERTIES
+print(prop.get('cities'))
+
+cities = prop.get('cities')
+api_key = prop.get('api_openweathermap_key')
 
 for city in cities:
     print(f"Checking weather data for {city}")
+
+    # Get the weather data from openweathermap.org
     weather_data = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}")
 
+    # convert api result into Json
     result = weather_data.json()
     print(result)
+
+    # Parse the api result into an insert query
     insert_query = helper.generate_insert_query(result)
     print(f"The insert query for {city} is {insert_query}")
     print("Making MySql connection")
+
     # Making MySql Connection
-    connection = sql.connect(host="localhost", user="root", passwd="Admin@123")
+    connection = sql.connect(
+        host=prop.get('api_openweathermap_key'),
+        user=prop.get('database_mysql_username'),
+        passwd=prop.get('database_mysql_password')
+    )
     print("MySql Connection established")
+
+    # Get the DB Cursor
     my_cursor = connection.cursor()
     my_cursor.execute(insert_query)
     print(f"Data for {city} inserted successfully")
+
+    # Commit the DB Changes
     connection.commit()
-
-
